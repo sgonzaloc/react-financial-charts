4fea4cc23450ed9e5c7b013914f719d9e1d7c3af
Gonzalo Sanchez Cano <gonzalo.sanchezcano@gmail.com>
2026-04-10 20:04:15 +0300

    add gann fan storiess to storybooks interactives

---
+++ packages/stories/src/interactives/gannFanlInteractive/index.stories.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import GannFanInteractive from "./GannFanInteractive";
+
+export default {
+    title: "Interactives/Gann Fan",
+};
+
+export const basic = () => <GannFanInteractive />;
