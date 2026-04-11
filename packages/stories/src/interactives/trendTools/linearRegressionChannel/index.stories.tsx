13d31a96a2ed14386f43322e2fd982069db7cb3e
Gonzalo Sanchez Cano <gonzalo.sanchezcano@gmail.com>
2026-04-11 23:25:16 +0300

    rename folders and title

---
+++ packages/stories/src/interactives/trendTools/linearRegressionChannel/index.stories.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import LinearRegressionChannelInteractive from "./LinearRegressionChannelInteractive";
+
+export default {
+    title: "Interactive Drawing Tools/Trend Tools/Linear Regression Channel",
+};
+
+export const basic = () => <LinearRegressionChannelInteractive />;
