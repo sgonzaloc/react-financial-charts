68bf61a3fc092c517e20fb8559ca7f2e7e01c6d7
Gonzalo Sanchez Cano <gonzalo.sanchezcano@gmail.com>
2026-04-12 13:53:56 +0300

    add FreehandBrush stories on interactiveActions

---
+++ packages/stories/src/interactives/shapes/freehandBrush/index.stories.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import FreehandBrushInteractive from "./FreehandBrushInteractive";
+
+export default {
+    title: "Interactive Drawing Tools/Interactive Actions/Freehand Brush",
+};
+
+export const basic = () => <FreehandBrushInteractive />;
