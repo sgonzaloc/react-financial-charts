52ce7fa621cb8af17441456aa4fd365810eb0456
Gonzalo Sanchez Cano <gonzalo.sanchezcano@gmail.com>
2026-04-12 16:15:32 +0300

    add full react 18 and react 19 support, and test both versions on stories

--- packages/stories/.storybook/main.js
+++ packages/stories/.storybook/main.js
@@ -1,6 +1,6 @@
 /** @type {import('@storybook/react-webpack5').StorybookConfig} */
 module.exports = {
-    addons: ["@storybook/addon-essentials", "@storybook/addon-webpack5-compiler-babel"],
+    addons: ["@storybook/addon-webpack5-compiler-babel"],
     stories: ["../src/**/*.stories.@(ts|tsx|mdx)"],
     framework: {
         name: "@storybook/react-webpack5",
